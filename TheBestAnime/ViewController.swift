d54a4ff37de45823250d9d30f980ca61b069f306
Azatbek <kanybekov66@gmak.com>
2019-02-03 22:17:41 +0600

    Initial Commit

---
+++ TheBestAnime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheBestAnime
+//
+//  Created by Azatbek Kanybekov on 2/3/19.
+//  Copyright © 2019 Azatbek Kanybekov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
